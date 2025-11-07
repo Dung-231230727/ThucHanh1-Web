1901cb70a3bcae852203fa4636658297d6abfdad
dung231230727@lms.utc.edu.vn <dung231230727@lms.utc.edu.vn>
2025-11-07 10:50:04 +0700

    Add project files.

---
+++ ThucHanh1/Program.cs
@@ -0,0 +1,39 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "admin_student_list",
+    pattern: "Admin/Student/List",
+    defaults: new { controller = "Student", action = "Index" });
+
+app.MapControllerRoute(
+    name: "admin_student_add",
+    pattern: "Admin/Student/Add",
+    defaults: new { controller = "Student", action = "Create" });
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
